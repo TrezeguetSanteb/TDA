a64c65b441c8c4d71397f4791f94f4ecccf40125
TrezeguetSanteb <strezeguet@fi.uba.ar>
2024-12-26 16:57:45 -0300

    correcciones parte1

--- Primera Parte/primera_parte.py
+++ Primera Parte/primera_parte.py
@@ -2,47 +2,58 @@ import sys
 import numpy as np
 
 def sofiaa(vec):
-    p= vec[0]
-    u= vec[len(vec)-1]
+    p = vec[0]
+    u = vec[-1]
 
     if p > u:
         vec = vec[1:]
-        return p, vec
+        return p, vec, "primera"
     else:
         vec = vec[:-1]
-        return u, vec
+        return u, vec, "ultima"
 
 def mateoo(vec):
-    p= vec[0]
-    u= vec[len(vec)-1]
+    p = vec[0]
+    u = vec[-1]
 
     if p < u:
         vec = vec[1:]
-        return p , vec
+        return p, vec, "primera"
     else:
         vec = vec[:-1]
-        return u, vec
-    
+        return u, vec, "ultima"
+
 def juego_greedy(vec):
-    
-    v_sofia = [] * (len(vec)//2)
-    v_mateo = [] * (len(vec)//2)
+    v_sofia = []
+    v_mateo = []
 
-    for i in range(0, len(vec)):
-        if (i%2==0 or i==0):
-            resultado, vec = sofiaa(vec) 
-            v_sofia.append(resultado)
+    for i in range(len(vec)):
+        if i % 2 == 0:
+            moneda, vec, posicion = sofiaa(vec)
+            v_sofia.append((moneda, posicion))
         else:
-            resultado, vec = mateoo(vec) 
-            v_mateo.append(resultado)
-    return sum(v_sofia)
+            moneda, vec, posicion = mateoo(vec)
+            v_mateo.append((moneda, posicion))
 
-def ParsearArchivo(archivo):
-    with open(archivo, 'r') as archivo:
-        contenido = archivo.read()
-        arr = [int(num) for num in contenido.split(';') if num.strip().isdigit()]
-    return arr
+    return v_sofia, v_mateo
 
 if __name__ == "__main__":
     archivo = sys.argv[1]
-    print(juego_greedy(ParsearArchivo(archivo)))
+    vec = np.loadtxt(archivo, delimiter=';', dtype=int)
+    v_sofia, v_mateo = juego_greedy(list(vec))
+    ganancia_sophia = sum([moneda for moneda, _ in v_sofia])
+    ganancia_mateo = sum([moneda for moneda, _ in v_mateo])
+
+    if ganancia_sophia <= ganancia_mateo:
+        print("Mateo gana")
+    else:
+        for i in range(len(v_sofia)):
+            moneda, posicion = v_sofia[i]
+            print(f"{posicion} moneda para Sophia({moneda})", end=", ")
+
+            if i < len(v_mateo):
+                moneda, posicion = v_mateo[i]
+            print(f"{posicion} moneda para Mateo({moneda})", end=", ")
+            print() 
+    
+        print(f"Ganancia de Sophia: {ganancia_sophia}")
